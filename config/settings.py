d1708b35544f13e1bae999fc0a44b2640e2bab31
TeeblaQ1 <oluwateeblaq@gmail.com>
2022-11-18 21:12:42 +0100

    fixed cors

--- config/settings.py
+++ config/settings.py
@@ -46,10 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
-    'corsheaders',
     'cloudinary',
     'authentication',
     'shop',
@@ -70,24 +70,24 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://toyosi-site.netlify.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "https://toyosi-site.netlify.app",
+# ]
 
 ROOT_URLCONF = 'config.urls'
 
--- shop/views.py
+++ shop/views.py
@@ -19,10 +19,11 @@ class ProductDetailAPIView(GenericAPIView):
     def get(self, request, id):
         user = request.user
         try:
-            user = User.objects.get(email=user)
             # add to user's recently viewed items
             product_instance = Product.objects.get(id=id)
-            product_instance.user_recent.add(user)
+            if not user.is_anonymous:
+                user = User.objects.get(email=user)
+                product_instance.user_recent.add(user)
             product = list(Product.objects.filter(id=id).values('name', 'slug', 'image', 'description', 'price', 'category'))[0]
 
             other_images = list(ProductImage.objects.filter(product=Product.objects.get(id=id)).values('other_image', 'image_description'))
