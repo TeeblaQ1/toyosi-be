c254e5b572913714da03eb131edbbe6be862dd23
TeeblaQ1 <oluwateeblaq@gmail.com>
2022-11-18 21:44:55 +0100

    updated cors allow all origins

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 CORS_ALLOW_ALL_ORIGINS = True
+
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:8080",
 #     "http://localhost:8000",
